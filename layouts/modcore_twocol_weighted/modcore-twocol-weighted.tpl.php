867af0dec8846d4a6c97e34404edd93a7993a8b9
Andrew Meyer <andrew@rewdy.com>
2013-10-18 17:29:09 -0500

    Added Panels layouts.

---
+++ layouts/modcore_twocol_weighted/modcore-twocol-weighted.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * ModCore Panels Layout
+ *
+ * @file
+ * Template for a 3 column panel layout.
+ *
+ * This template provides a three column panel display layout, with
+ * each column roughly equal in width.
+ *
+ * Variables:
+ * - $id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ *   panel of the layout. This layout supports the following sections:
+ *   - $content['left']: Content in the left column.
+ *   - $content['middle']: Content in the middle column.
+ *   - $content['right']: Content in the right column.
+ */
+?>
+
+<div <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?> class="row">
+	<div class="g8">
+		<?php print $content['left']; ?>
+	</div>
+	<div class="g4">
+		<?php print $content['right']; ?>
+	</div>
+</div>
